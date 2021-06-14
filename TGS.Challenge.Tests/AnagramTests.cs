f95b3633d7028ef3ea3057dd768411a613499dfd
Adrian Brink <adrian.brink@outlook.com>
2021-06-14 18:55:42 +0200

    anagrams

--- TGS.Challenge.Tests/AnagramTests.cs
+++ TGS.Challenge.Tests/AnagramTests.cs
@@ -3,66 +3,74 @@ using NUnit.Framework;
 
 namespace TGS.Challenge.Tests
 {
-  [TestFixture()]
-  public class AnagramTests
-  {
-    private readonly Anagram _anagram;
-
-    public AnagramTests()
+    [TestFixture()]
+    public class AnagramTests
     {
-      this._anagram = new Anagram();
+        private readonly Anagram _anagram;
+
+        public AnagramTests()
+        {
+            this._anagram = new Anagram();
+        }
+
+        [Test()]
+        public void Word1_IsRequired()
+        {
+            Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams(string.Empty, "ABC"));
+        }
+
+        [Test()]
+        public void Word2_IsRequired()
+        {
+            Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams("ABC", string.Empty));
+        }
+
+        [Test()]
+        public void Dormitory_IsAnagram_Dirty_room()
+        {
+            var result = _anagram.AreAnagrams("Dormitory", "Dirty_room");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void Funeral_Is_NOT_Anagram_Reel_fun()
+        {
+            var result = _anagram.AreAnagrams("Funeral", "Reel fun");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test()]
+        public void School_master_IsAnagram_The_classroom()
+        {
+            var result = _anagram.AreAnagrams("School master?!", "!?The classroom");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void Listen_Is_NOT_Anagram_Silence()
+        {
+            var result = _anagram.AreAnagrams("Listen", "Silence");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test()]
+        public void Funeral_IsAnagram_Real_fun()
+        {
+            var result = _anagram.AreAnagrams("Funeral", "Real fun");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void I_Have_A_Large_Penis_IsAnagram_He_Is_Plain_Average()
+        {
+            var result = _anagram.AreAnagrams("I have a large penis", "He is plain average");
+
+            Assert.IsTrue(result);
+        }
     }
-
-    [Test()]
-    public void Word1_IsRequired()
-    {
-      Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams(string.Empty, "ABC"));
-    }
-
-    [Test()]
-    public void Word2_IsRequired()
-    {
-      Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams("ABC", string.Empty));
-    }
-
-    [Test()]
-    public void Dormitory_IsAnagram_Dirty_room()
-    {
-      var result = _anagram.AreAnagrams("Dormitory", "Dirty_room");
-
-      Assert.IsTrue(result);
-    }
-
-    [Test()]
-    public void Funeral_IsAnagram_Reel_fun()
-    {
-      var result = _anagram.AreAnagrams("Funeral", "Reel fun");
-
-      Assert.IsTrue(result);
-    }
-
-    [Test()]
-    public void School_master_IsAnagram_The_classroom()
-    {
-      var result = _anagram.AreAnagrams("School master?!", "!?The classroom");
-
-      Assert.IsTrue(result);
-    }
-    
-    [Test()]
-    public void Listen_Is_NOT_Anagram_Silence()
-    {
-      var result = _anagram.AreAnagrams("Listen", "Silence");
-
-      Assert.IsFalse(result);
-    }
-
-    [Test()]
-    public void Funeral_IsAnagram_Real_fun()
-    {
-      var result = _anagram.AreAnagrams("Funeral", "Real fun");
-
-      Assert.IsTrue(result);
-    }
-  }
 }
--- TGS.Challenge/Anagram.cs
+++ TGS.Challenge/Anagram.cs
@@ -1,30 +1,79 @@
+using System;
+
 namespace TGS.Challenge
 {
-  /*
-        Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
-        one another return true, else return false
+    /*
+          Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
+          one another return true, else return false
 
-        "Anagram": An anagram is a type of word play, the result of rearranging the letters of a word or
-        phrase to produce a new word or phrase, using all the original letters exactly once; for example
-        orchestra can be rearranged into carthorse.
+          "Anagram": An anagram is a type of word play, the result of rearranging the letters of a word or
+          phrase to produce a new word or phrase, using all the original letters exactly once; for example
+          orchestra can be rearranged into carthorse.
 
-        areAnagrams("horse", "shore") should return true
-        areAnagrams("horse", "short") should return false
+          areAnagrams("horse", "shore") should return true
+          areAnagrams("horse", "short") should return false
 
-        NOTE: Punctuation, including spaces should be ignored, e.g.
+          NOTE: Punctuation, including spaces should be ignored, e.g.
 
-        horse!! shore = true
-        horse  !! shore = true
-          horse? heroes = true
+          horse!! shore = true
+          horse  !! shore = true
+            horse? heroes = true
 
-        There are accompanying unit tests for this exercise, ensure all tests pass & make
-        sure the unit tests are correct too.
-     */
+          There are accompanying unit tests for this exercise, ensure all tests pass & make
+          sure the unit tests are correct too.
+       */
     public class Anagram
     {
-      public bool AreAnagrams(string word1, string word2)
-      {
-        return false;
-      }
+        public bool AreAnagrams(string word1, string word2)
+        {
+            ValidateConstraints(word1, word2);
+
+            word1 = RemovePunctuationAndSpaces(word1);
+            word2 = RemovePunctuationAndSpaces(word2);
+
+            var word1Sorted = SortLettersAscending(word1);
+            var word2Sorted = SortLettersAscending(word2);
+
+            return word1Sorted == word2Sorted;
+        }
+
+        private void ValidateConstraints(string word1, string word2)
+        {
+            if (string.IsNullOrEmpty(word1))
+            {
+                throw new ArgumentException("Word 1 is required");
+            }
+
+            if (string.IsNullOrEmpty(word2))
+            {
+                throw new ArgumentException("Word 2 is required");
+            }
+        }
+
+        private string RemovePunctuationAndSpaces(string value)
+        {
+            var result = string.Empty;
+            foreach (var charValue in value)
+            {
+                if (!char.IsPunctuation(charValue) && charValue != ' ')
+                {
+                    result += charValue;
+                }
+            }
+            return result;
+        }
+
+        private string SortLettersAscending(string value)
+        {
+            char[] charArray = value.ToLower().ToCharArray();
+            Array.Sort(charArray);
+
+            var result = string.Empty;
+            foreach (var charValue in charArray)
+            {
+                result += charValue;
+            }
+            return result;
+        }
     }
 }
