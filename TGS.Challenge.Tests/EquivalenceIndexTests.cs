1d6d46dd4bf93e03ccb93a914b553ded5080f6a0
Adrian Brink <adrian.brink@outlook.com>
2021-06-14 19:10:59 +0200

    equivalence index

--- TGS.Challenge.Tests/EquivalenceIndexTests.cs
+++ TGS.Challenge.Tests/EquivalenceIndexTests.cs
@@ -1,47 +1,54 @@
 using NUnit.Framework;
+using System;
 
 namespace TGS.Challenge.Tests
 {
-  [TestFixture()]
-  public class EquivalenceIndexTests
-  {
-    private readonly EquivalenceIndex _equivalenceIndex;
-
-    public EquivalenceIndexTests()
+    [TestFixture()]
+    public class EquivalenceIndexTests
     {
-      this._equivalenceIndex= new EquivalenceIndex();
+        private readonly EquivalenceIndex _equivalenceIndex;
+
+        public EquivalenceIndexTests()
+        {
+            this._equivalenceIndex = new EquivalenceIndex();
+        }
+
+        [Test()]
+        public void Returns_Index_ForValidNumberSequence()
+        {
+            var index = _equivalenceIndex.Find(new int[] { 1, 2, 3, 4, 5, 7, 8, 10, 12 });
+
+            Assert.AreEqual(6, index);
+        }
+
+        [Test()]
+        public void Retruns_Neg1_ForInvalidNumberSequence()
+        {
+            var index = _equivalenceIndex.Find(new int[] { 2, 2, 3, 4, 3, 74, 8, 10, 12 });
+
+            Assert.AreEqual(-1, index);
+        }
+
+        [Test()]
+        public void Returns_Index_ForValidNumberSequence_LeftWeighted()
+        {
+            var index = _equivalenceIndex.Find(new int[] { 10, 11, 3, 1, 1, 1, 9, 9 });
+
+            Assert.AreEqual(2, index);
+        }
+
+        [Test()]
+        public void Returns_Index_ForValidNumberSequence_RightWeighted()
+        {
+            var index = _equivalenceIndex.Find(new int[] { 4, 3, 5, 6, 7, 5, 10, 15 });
+
+            Assert.AreEqual(5, index);
+        }
+
+        [Test()]
+        public void Max_Array_Length_Throws_ArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _equivalenceIndex.Find(new int[100001]));
+        }
     }
-
-    [Test()]
-    public void Returns_Index_ForValidNumberSequence()
-    {
-        var index = _equivalenceIndex.Find(new int[] { 1, 2, 3, 4, 5, 7, 8, 10, 12 });
-
-        Assert.AreEqual(6, index);
-    }
-
-    [Test()]
-    public void Retruns_Neg1_ForInvalidNumberSequence()
-    {
-      var index = _equivalenceIndex.Find(new int[] { 2, 2, 3, 4, 3, 74, 8, 10, 12 });
-
-      Assert.AreEqual(-1, index);
-    }
-
-    [Test()]
-    public void Returns_Index_ForValidNumberSequence_LeftWeighted()
-    {
-        var index = _equivalenceIndex.Find(new int[] { 10, 11, 3, 1, 1, 1, 9, 9 });
-
-        Assert.AreEqual(2, index);
-    }
-
-    [Test()]
-    public void Returns_Index_ForValidNumberSequence_RightWeighted()
-    {
-        var index = _equivalenceIndex.Find(new int[] { 4, 3, 5, 6, 7, 5, 10, 15 });
-
-        Assert.AreEqual(5, index);
-    }
-  }  
 }
--- TGS.Challenge/EquivalenceIndex.cs
+++ TGS.Challenge/EquivalenceIndex.cs
@@ -1,25 +1,57 @@
+using System;
+
 namespace TGS.Challenge
 {
-  /*
-       Given a zero-based integer array of length N, the equivalence index (i) is the index where the sum of all the items to the left of the index
-       are equal to the sum of all the items to the right of the index.
+    /*
+         Given a zero-based integer array of length N, the equivalence index (i) is the index where the sum of all the items to the left of the index
+         are equal to the sum of all the items to the right of the index.
 
-       Constraints: 0 <= N <= 100 000
+         Constraints: 0 <= N <= 100 000
 
-       Example: Given the following array [1, 2, 3, 4, 5, 7, 8, 10, 12]
-       Your program should output "6" because 1 + 2 + 3 + 4 + 5 + 7 = 10 + 12
+         Example: Given the following array [1, 2, 3, 4, 5, 7, 8, 10, 12]
+         Your program should output "6" because 1 + 2 + 3 + 4 + 5 + 7 = 10 + 12
 
-       If no index exists then output -1
+         If no index exists then output -1
 
-       There are accompanying unit tests for this exercise, ensure all tests pass & make
-        sure the unit tests are correct too.
-     */
+         There are accompanying unit tests for this exercise, ensure all tests pass & make
+          sure the unit tests are correct too.
+       */
 
     public class EquivalenceIndex
     {
-      public int Find(int[] numbers)
-      {
-        return -99;
-      }
+        public int Find(int[] numbers)
+        {
+            ValidateConstraints(numbers);
+
+            var result = -1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var leftArray = 0;
+                for (int l = 0; l < i; l++)
+                {
+                    leftArray += numbers[l];
+                }
+
+                var rightArray = 0;
+                for (int r = numbers.Length - 1; r > i; r--)
+                {
+                    rightArray += numbers[r];
+                }
+
+                if (leftArray == rightArray)
+                {
+                    return i;
+                }
+            }
+            return result;
+        }
+
+        private void ValidateConstraints(int[] numbers)
+        {
+            if (numbers.Length > 100000)
+            {
+                throw new ArgumentOutOfRangeException("Array length must be less than than 100 000");
+            }
+        }
     }
 }
